b01b48c7dd0b2ab05d71bc1172f40be6d44b656b
rhsabbir1 <rhsabbir090@gmai.com>
2023-05-23 12:52:56 +0600

    footer added

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../pages/shard/Footer/Footer';
 
 const Main = () => {
     return (
-        <div>
-            <button className="btn">Button</button>
-            <button className="btn btn-primary">Button</button>
-            <button className="btn btn-secondary">Button</button>
-            <button className="btn btn-accent">Button</button>
-            <button className="btn btn-ghost">Button</button>
-            <button className="btn btn-link">Button</button>
-        </div>
+        <>
+        <Outlet></Outlet>
+        <Footer></Footer>
+        </>
     );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { RouterProvider} from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 import routers from './Routers/Routers'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={routers}></RouterProvider>
+  <div className='max-w-screen-xl mx-auto'>
+    <RouterProvider router={routers}></RouterProvider>
+  </div>
+
 )
