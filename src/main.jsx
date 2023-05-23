de5054926ea62d106c8c0cbb4298ca0356228be7
rhsabbir1 <rhsabbir090@gmai.com>
2023-05-23 12:28:12 +0600

    tailwind css aded

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-
-
-const router = createBrowserRouter([
-  {
-    path:'/',
-    element:<App></App>
-  }
-])
+import { RouterProvider} from 'react-router-dom'
+import routers from './Routers/Routers'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router}></RouterProvider>
+  <RouterProvider router={routers}></RouterProvider>
 )
