8addd63c1e0e78a517219e5050797fe8d45e9677
rhsabbir1 <rhsabbir090@gmai.com>
2023-05-23 12:58:19 +0600

    navber added

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../pages/shard/Footer/Footer';
+import Navber from '../pages/shard/Navber/Navber';
 
 const Main = () => {
     return (
         <>
+        <Navber></Navber>
         <Outlet></Outlet>
         <Footer></Footer>
         </>
